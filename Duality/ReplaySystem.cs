16b65372aeef16fe13ba152934b2dcdda5b93c8c
Andrea <roundcrisis@gmail.com>
2014-02-14 19:35:00 +0000

    kinda plays

--- Duality/ReplaySystem.cs
+++ Duality/ReplaySystem.cs
@@ -1,39 +1,85 @@
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using Duality.Resources;
 
 namespace Duality
 {
 	public class ReplaySystem
 	{
+		private enum ReplaySystemStatus
+		{
+			Recording,
+			StartPlaying,
+			Playing,
+			Idle
+		}
+
 		private readonly BackgroundWorker _backgroundWorker = new BackgroundWorker();
 		const string ReplaySavePath = "scene";
 		const string ReplayDirectory = "replay";
+		private bool _isPlaying;
+		private static ReplaySystemStatus _replaySystemStatus;
+
 		public ReplaySystem()
 		{
 			_backgroundWorker.DoWork += SaveCurrentScene;
+			_replaySystemStatus = ReplaySystemStatus.Idle;
 		}
 
-		public void SaveFrame(Scene scene)
+		public void StartRecording()
 		{
-			if (!_backgroundWorker.IsBusy)
-				_backgroundWorker.RunWorkerAsync(scene);
+			if (_replaySystemStatus == ReplaySystemStatus.Idle)
+				_replaySystemStatus = ReplaySystemStatus.Recording;
+
 		}
 
-		public void Play()
+		public void StopRecording()
 		{
-			foreach (var fileName in Directory.GetFiles(ReplayDirectory).OrderByDescending(x=> x))
+			if (_replaySystemStatus == ReplaySystemStatus.Recording)
 			{
-				Scene.Current = Resource.Load<Scene>(fileName);
+				_backgroundWorker.RunWorkerCompleted += delegate { _replaySystemStatus = ReplaySystemStatus.Idle; };
+				_backgroundWorker.DoWork -= SaveCurrentScene;
 			}
 		}
+		private string[] _sceneFileNamess;
+		private int _currentSceneIndex;
+		public void Play()
+		{
+			switch (_replaySystemStatus)
+			{
+				case ReplaySystemStatus.Playing:
+					Scene.Current = Resource.Load<Scene>(_sceneFileNamess[_currentSceneIndex]);
+					_currentSceneIndex++;
+					if (_currentSceneIndex <= _sceneFileNamess.Length)
+					{
+						_currentSceneIndex = 0;
+						Log.Editor.Write("Finished playback");
+					}
+					break;
+				case ReplaySystemStatus.Idle:
+					_replaySystemStatus = ReplaySystemStatus.Playing;
+					_sceneFileNamess = Directory.GetFiles(ReplayDirectory).ToArray();
+					_currentSceneIndex = 0;
+					break;
+			}
 
+			
+		}
+
+		public void Update()
+		{
+			if (_replaySystemStatus == ReplaySystemStatus.Recording)
+			{
+				if (!_backgroundWorker.IsBusy)
+					_backgroundWorker.RunWorkerAsync();
+			}
+		}
 		private void SaveCurrentScene(object sender, DoWorkEventArgs e)
 		{
-			Stream stream = File.Create(Path.Combine(ReplayDirectory,ReplaySavePath+Time.MainTimer.Milliseconds+".res"));
-			var currentScene = (Scene) e.Argument;
-			currentScene.Save(stream);
+			Stream stream = File.Create(Path.Combine(ReplayDirectory, ReplaySavePath + Time.MainTimer.Milliseconds + ".res"));
+			Scene.Current.Save(stream);
 		}
 	}
 }
